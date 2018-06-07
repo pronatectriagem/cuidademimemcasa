2ac5e33d1e23382d11a1064c62f423e82b5fdd0a
pronatectriagem <40012710+pronatectriagem@users.noreply.github.com>
2018-06-07 05:05:46 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 <body>
 	<?php echo 'Ola Mundo!'?>
 	<h1><b>Pagina Principal em construcao!!</b></h1>
-	<h2>Este site encontra-se hospedado na MICROSOFT AZURE</h2>
+	<h2>Este site encontra-se hospedado no MICROSOFT AZURE</h2>
 </body>
 </html>
