96616f2eeb73c060bf905b274db71cd862a9451f
pronatectriagem <40012710+pronatectriagem@users.noreply.github.com>
2018-06-07 04:54:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>CuidademimemCasa</title>
+</head>
+<body>
+	<?php echo 'Ola Mundo!'?>
+	<h1><b>Pagina Principal em construcao!!</b></h1>
+	<h2>Este site encontra-se hospedado na MICROSOFT AZURE</h2>
+
+</body>
+</html>
